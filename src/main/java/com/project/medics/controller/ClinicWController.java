cd3430a9609fc27a5b497c37784f85fea7a22eeb
HyunjunJang <hyunjun301@gmail.com>
2024-03-19 20:21:42 +0900

    대문자수정-현준

--- src/main/java/com/project/medics/controller/ClinicWController.java
+++ src/main/java/com/project/medics/controller/ClinicWController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.medics.clinicW.service.ClinicWService;
 import com.project.medics.command.ClinicWVO;
