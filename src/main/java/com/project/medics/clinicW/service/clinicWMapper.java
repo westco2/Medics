1e8dc4c3e51d9b17235312f71b1c7e0a5dfcac10
WonJun <choiggl89@gmail.com>
2024-03-19 19:23:52 +0900

    원커밋

--- src/main/java/com/project/medics/clinicW/service/clinicWMapper.java
+++ src/main/java/com/project/medics/clinicW/service/clinicWMapper.java
@@ -1,8 +1,20 @@
 package com.project.medics.clinicW.service;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public interface clinicWMapper {
+import com.project.medics.command.ClinicWVO;
 
+
+@Mapper
+public interface ClinicWMapper {
+
+	public int regist(ClinicWVO vo);
+	public ArrayList<ClinicWVO> getPage();
+	public ArrayList<ClinicWVO> getStatus();
+	public ArrayList<ClinicWVO> getReview();
+	public ClinicWVO getClinicWUpdate(int cli_ntc_sn);
+//	public ClinicWVO getDetail(int cli_ntc_sn);
+	public void delete(int cli_sn);
 }
--- src/main/java/com/project/medics/command/clinicWVO.java
+++ src/main/java/com/project/medics/command/clinicWVO.java
@@ -1,5 +1,7 @@
 package com.project.medics.command;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +11,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class clinicWVO {
-
-	private String name;	
+public class ClinicWVO {
+	//Regist 테이블
+	private int cli_ntc_sn; //번호
+    private String cli_clsf_nm; //분류
+    private String cli_ntc_ttl; //제목
+    private String cli_ntc_cn; //내용
+    private String cli_use_plc_nm; //장소
+    private Date cli_ntc_ymd; //날짜
+    private String cli_prgrs_hr; //진행시간
+    private int cli_now_nope; //현재인원
+    private int cli_max_nope; //최대인원
+    private String cli_instr_nm; //진행자
+    private String cli_rmrk_cn; //비고
+    
+    //Apply 테이블
+    private int cli_sn; //신청번호
+    private	String cli_status; //상태
+    
+    //Review 테이블
+    private String cli_wri_ttl; //제목
+    private String cli_wri_cn; //내용
+    private String cli_wri_yn; //여부
+    
+    //가데이터
+    private String studentid;
 }
