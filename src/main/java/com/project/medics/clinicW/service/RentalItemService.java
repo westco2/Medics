a01750a02d135af3417558aee787c668bd79fd25
Serven <https://github.com/westco2/example1.git>
2024-03-21 17:12:31 +0900

    plz

--- src/main/java/com/project/medics/clinicW/service/RentalItemService.java
+++ src/main/java/com/project/medics/clinicW/service/RentalItemService.java
@@ -4,6 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+<<<<<<< HEAD
 public class RentalItemService {
+=======
+public interface RentalItemService {
+    List<RentalVO> getAllItems();
+    List<RentalVO> getItemById();
+    List<RentalVO> updateItem();
+
+}
+>>>>>>> 3483abd (안되면 뒤졌다)
 
 }
--- src/main/java/com/project/medics/clinicW/service/RentalItemServiceImpl.java
+++ src/main/java/com/project/medics/clinicW/service/RentalItemServiceImpl.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.project.medics.clinicW.service;
 //
 //import java.util.ArrayList;
@@ -48,3 +49,38 @@
 //        itemList.removeIf(item -> item.getStudentId().equals(itemId));
 //    }
 //}
+=======
+package com.project.medics.clinicW.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.medics.command.RentalVO;
+
+@Service(value="rentalService")
+public class RentalItemServiceImpl implements RentalItemService {
+
+	@Autowired
+	private RentalJYMapper rentalJYMapper; 
+    
+    @Override
+    public List<RentalVO> getAllItems() {
+        return rentalJYMapper.rentallist();
+    }
+    
+    @Override
+    public List<RentalVO> getItemById() {
+    	return rentalJYMapper.rentalapp();
+    }
+
+    @Override
+    public List<RentalVO> updateItem() {
+        return rentalJYMapper.appovalc();
+    }
+
+
+
+}
+>>>>>>> 3483abd (안되면 뒤졌다)
